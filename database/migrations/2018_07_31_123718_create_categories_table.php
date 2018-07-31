b75e04266e6aec92738dbd4c10490945a9aacd82
adrian <adrian.sebuliba1@gmail.com>
2018-07-31 15:44:49 +0300

    chore(migrations): add migrations

---
+++ database/migrations/2018_07_31_123718_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->nullable()->unsigned();
+            $table->string('name');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::dropIfExists('categories');
+    }
+}
